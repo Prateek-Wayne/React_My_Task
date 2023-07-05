a2031e8786266b09bb32241a4a1b92aa76bf662c
Prateek Wayne <prateek.12016050@lpu.in>
2023-07-05 09:59:46 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/SingleTodo.tsx

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,54 +1,67 @@
-import React from 'react';
+import React from "react";
 import { AiFillDelete } from "react-icons/ai";
-// import { AiFillEdit} from "react-icons/ai";
+import { AiFillEdit } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 interface Todo {
-    id: number;
-    todo: string;
-    isDone: boolean;
+  id: number;
+  todo: string;
+  isDone: boolean;
 }
 type Props = {
-    todo: Todo;
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-    const handleDone = (id: number) => {
-        setTodos(
-          todos.map((todo) =>
-            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
-          )
-        );
-        
-    };
-    const handleDelete = (id: number) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    };
-    return (
-        
-            <form className="todos__single">
-                {   
-                    
-                    todo.isDone ? (
-                        <s className="todos_single_text">{todo.todo}</s>
-                    ) : (
-                        <span className="todos_single_text">{todo.todo}</span>
-                    ) 
-                   
-                }
-                {/* <span className="todos_single_text">{todo.todo}</span> */}
-                <div>
-                    <span className="icon" onClick={()=> handleDelete(todo.id)} > <AiFillDelete /> </span>
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  //.........editing
+  const handleEdit = (id: number) => {
+    let a = prompt("Edit Input");
+    let name = "temp";
+    {
+      if (a != null) {
+        name = a;
+      }
+    }
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: name } : todo))
+    );
+  };
+  //....................
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  return (
+    <form className="todos__single">
+      {todo.isDone ? (
+        <s className="todos_single_text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single_text">{todo.todo}</span>
+      )}
+      {/* <span className="todos_single_text">{todo.todo}</span> */}
+      <div>
+        <span className="icon" onClick={() => handleEdit(todo.id)}>
+          {" "}
+          <AiFillEdit />{" "}
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          {" "}
+          <MdDone />{" "}
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          {" "}
+          <AiFillDelete />{" "}
+        </span>
+      </div>
+    </form>
+  );
+};
 
-                    
-
-                    <span className="icon" onClick={()=> handleDone(todo.id)} > <MdDone /> </span>
-                    {/* <span className="icon"> <AiFillEdit /> </span> */}
-                </div>
-            </form>
-        
-    )
-}
-
-export default SingleTodo
+export default SingleTodo;
